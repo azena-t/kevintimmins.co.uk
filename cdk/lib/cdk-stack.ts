13b8dc3efae988dbda9fb511224073345cbc4ebd
Kevin Timmins <kevin.timmins@socralogic.co.uk>
2021-04-23 15:34:27 +0100

    Added the config to hook into Route53 and put a custom domain on the front

--- cdk/lib/cdk-stack.ts
+++ cdk/lib/cdk-stack.ts
@@ -1,28 +1,45 @@
 import * as cdk from '@aws-cdk/core';
+import {Duration} from '@aws-cdk/core';
 import {Bucket, BucketAccessControl} from "@aws-cdk/aws-s3";
 import {BucketDeployment, Source} from "@aws-cdk/aws-s3-deployment";
-import {Distribution, OriginAccessIdentity, PriceClass, CachePolicy} from "@aws-cdk/aws-cloudfront";
+import {
+  CachePolicy,
+  Distribution,
+  OriginAccessIdentity,
+  PriceClass,
+  SecurityPolicyProtocol
+} from "@aws-cdk/aws-cloudfront";
 import {S3Origin} from "@aws-cdk/aws-cloudfront-origins";
 import * as path from "path";
-import {Duration} from "@aws-cdk/core";
+import {ARecord, HostedZone, RecordTarget} from "@aws-cdk/aws-route53";
+import {CloudFrontTarget} from "@aws-cdk/aws-route53-targets";
+import {DnsValidatedCertificate} from "@aws-cdk/aws-certificatemanager";
 
 export class CdkStack extends cdk.Stack {
   constructor(scope: cdk.Construct, id: string, props?: cdk.StackProps) {
     super(scope, id, props);
 
+    const domainName = "kevintimmins.co.uk";
+    const siteSubDomain = "www"
+
+    const zone = HostedZone.fromLookup(this, 'Zone', { domainName: domainName });
+    const siteDomain = siteSubDomain + '.' + domainName;
+    new cdk.CfnOutput(this, 'Site', { value: 'https://' + siteDomain });
+
     const bucket = new Bucket(this, 'kevintimmins.co.uk', {
       accessControl: BucketAccessControl.PRIVATE
-    });
-
-    new BucketDeployment(this, 'BucketDeployment', {
-      destinationBucket: bucket,
-      sources: [Source.asset(path.resolve(__dirname, '../../web-app/build'))]
     });
 
     const originAccessIdentity = new OriginAccessIdentity(this, 'OriginAccessIdentity');
     bucket.grantRead(originAccessIdentity);
 
-    new Distribution(this, 'Distribution', {
+    const certificate = new DnsValidatedCertificate(this, 'SiteCertificate', {
+      domainName: siteDomain,
+      hostedZone: zone,
+      region: 'us-east-1', // Cloudfront only checks this region for certificates.
+    });
+
+    const distribution = new Distribution(this, 'SiteDistribution', {
       defaultRootObject: 'index.html',
       defaultBehavior: {
         origin: new S3Origin(bucket, {originAccessIdentity}),
@@ -30,7 +47,22 @@ export class CdkStack extends cdk.Stack {
           maxTtl: Duration.seconds(60)
         })
       },
+      certificate: certificate,
+      domainNames: [siteDomain],
       priceClass: PriceClass.PRICE_CLASS_100
-    })
+    });
+
+    new ARecord(this, 'SiteAliasRecord', {
+      recordName: siteDomain,
+      target: RecordTarget.fromAlias(new CloudFrontTarget(distribution)),
+      zone
+    });
+
+    new BucketDeployment(this, 'BucketDeployment', {
+      destinationBucket: bucket,
+      sources: [Source.asset(path.resolve(__dirname, '../../web-app/build'))],
+      distribution,
+      distributionPaths: ['/*']
+    });
   }
 }
